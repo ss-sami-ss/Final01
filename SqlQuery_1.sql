6960242c4bcfeea6dcd08dce243e598b9b92ede5
Saman Gh <so_sami_so@yahoo.com>
2019-12-28 16:58:12 +0330

    Database Creation

---
+++ SqlQuery_1.sql
@@ -0,0 +1,14 @@
+﻿create database Contact
+go
+use Contact
+go
+create table dbo.Person
+(
+	Id int primary key identity(1,1),
+	PhoneNumber nvarchar(11),
+	TelephoneNumber nvarchar(11),
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	IdentityCode nvarchar(10)
+	
+);
